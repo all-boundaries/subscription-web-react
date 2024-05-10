deb004c66fd6729271e45338098001288be56720
Rafael Ferreira <rafa@rafasf.com>
2024-05-10 10:36:33 +0800

    Setup test framework essentials

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [!process.env.VITEST && remix(), tsconfigPaths()],
+  test: {
+    environment: "happy-dom", // or 'jsdom', 'node'
+  },
 });
